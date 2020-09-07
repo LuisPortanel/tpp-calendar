48219543c4bf68eee790bb528ca09bbe666e9b7e
Luis Portanel <luis@portanel.com>
2020-09-07 04:12:44 -0500

    TPP-5: Add moment context for MUI Date Picker.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,29 @@
 // @flow
 import React from 'react'
 import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
-import { type Store as ReduxStore } from 'redux'
-import { type State, type Action } from '../redux/reducers'
-import { type BrowserHistory } from 'history'
+import { BrowserRouter, type RouterHistory } from 'react-router-dom'
+import { MuiPickersUtilsProvider } from '@material-ui/pickers'
+import MomentUtils from '@date-io/moment'
 import Nav from './Nav'
 import Routes from '../routes'
+
+import { type Store as ReduxStore } from 'redux'
+import { type State, type Action } from '../redux/reducers'
 
 import './App.scss'
 
 type AppType = {
   store: ReduxStore<State, Action>,
-  browserHistory: BrowserHistory
+  browserHistory: RouterHistory
 }
 
 const App = ({ store, browserHistory }: AppType) => <Provider store={store}>
-  <BrowserRouter>
-    <Nav />
-    <Routes />
-  </BrowserRouter>
+  <MuiPickersUtilsProvider utils={MomentUtils}>
+    <BrowserRouter>
+      <Nav />
+      <Routes />
+    </BrowserRouter>
+  </MuiPickersUtilsProvider>
 </Provider>
 
 export default App
